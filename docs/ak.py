a585b272c1c4827b22cedc448f7114939d35ce35
Horoyoi-san <185492733+horoyoi-san@users.noreply.github.com>
2025-11-28 17:44:30 +0700

    rp

---
+++ docs/ak.py
@@ -0,0 +1,19 @@
+from mitmproxy import http
+from urllib.parse import urlparse, parse_qsl, urlencode
+import requests
+import ssl
+import json
+
+
+def request(flow: http.HTTPFlow) -> None:
+    if "gryphline.com" in flow.request.pretty_url:
+        if flow.request.method == "CONNECT":
+            return
+        if "/get_latest_resources" in flow.request.pretty_url:
+            return
+        flow.request.scheme = "http"
+        flow.request.cookies.update(
+            {"OriginalHost": flow.request.host, "OriginalUrl": flow.request.url}
+        )
+        flow.request.host = "127.0.0.1"
+        flow.request.port = 5000
