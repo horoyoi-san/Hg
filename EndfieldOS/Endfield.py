89e4306193d04506693f152225ba0cade0356dc1
Horoyoi-san <korrawitcompara@gmail.com>
2026-04-15 01:51:36 +0700

    Update Endfield.py

--- EndfieldOS/Endfield.py
+++ EndfieldOS/Endfield.py
@@ -7,7 +7,7 @@ from datetime import datetime, timezone
 # ================= Webhook =================
 webhook_urls = [
     os.environ.get("WEBHOOK"),
-    os.environ.get("WEBHOOK2"),
+    os.environ.get("WEBHOOK1"),
     os.environ.get("WEBHOOK2"),
 ]
 
