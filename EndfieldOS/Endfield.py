b05d02e9e897ca3038ffba0899fe21f3297a86e5
Horoyoi-san <korrawitcompara@gmail.com>
2026-05-07 11:10:51 +0700

    up

--- EndfieldOS/Endfield.py
+++ EndfieldOS/Endfield.py
@@ -13,7 +13,7 @@ webhook_urls = [
 
 # ================= Branding =================
 BOT_NAME = "Endfield PROD"
-BOT_ICON = "https://github.com/horoyoi-san/Hg/blob/webhook/assets/endfield.png"
+BOT_ICON = "https://raw.githubusercontent.com/horoyoi-san/Hg/refs/heads/webhook/assets/endfield.png"
 
 # ================= API =================
 IMAGE_API = "https://raw.githubusercontent.com/horoyoi-san/Endfield/refs/heads/main/output/akEndfield/launcher/web/6/main_bg_image/th-th/all.json"
