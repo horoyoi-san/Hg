a9d7299e7e7693fe1cb08f3f90dd7f960ac9e71c
Horoyoi-san <185492733+horoyoi-san@users.noreply.github.com>
2026-08-18 22:34:08 +0700

    1.5

--- Timeline/Timeline Endfield.py
+++ Timeline/Timeline Endfield.py
@@ -6,16 +6,16 @@ CONFIG = {
     "start_dates": {
         "Drip": bangkok_datetime(2026, 7, 10, 18, 0),
         "BETA": bangkok_datetime(2026, 7, 10, 10, 0),
-        "Live": bangkok_datetime(2026, 8, 28, 18, 0),
+        "Live": bangkok_datetime(2026, 8, 21, 19, 30),
         "Predownload": bangkok_datetime(2026, 9, 1, 13, 0),
         "Release": bangkok_datetime(2026, 9, 3, 10, 0),
     },
     "intervals": {
-        "Drip": 49,
-        "BETA": 49,
-        "Live": 49,
-        "Predownload": 49,
-        "Release": 49,
+        "Drip": 42,
+        "BETA": 42,
+        "Live": 42,
+        "Predownload": 42,
+        "Release": 42,
     },
     "phase_labels": {
         "Drip": "Drip",
